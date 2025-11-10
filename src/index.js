6aa7e2327667bdc9cc34703a728aa0bafe9fa151
Kayedimo <yessenia.dimo@gmail.com>
2025-11-10 21:31:25 +0100

    Nuevo .env

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
+import { pool } from './db.js';
+
+try {
+  const [rows] = await pool.query('SELECT 1');
+  console.log('✅ Conexión a la base de datos exitosa');
+} catch (error) {
+  console.error('❌ Error de conexión:', error.message);
+}
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`)
 })
