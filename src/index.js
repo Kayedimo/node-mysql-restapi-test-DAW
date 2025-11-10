a6b6846af5bba75ec0411e094dd70b8cf218615a
Kayedimo <yessenia.dimo@gmail.com>
2025-11-10 15:23:56 +0100

    Corrijo host y puerto para Railway

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ dotenv.config()
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, '0.0.0.0',() => {
-    console.log('Server running on port', PORT)
+    console.log('Server running on port ${PORT}')
 })
 
 
