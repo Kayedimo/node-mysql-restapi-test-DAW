7c38ac06a0f0a542cf2b1c2f64e104938f37ccb7
Kayedimo <yessenia.dimo@gmail.com>
2025-11-10 15:34:11 +0100

    Corrijo puerto para Railway

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, '0.0.0.0',() => {
-    console.log('Server running on port ${PORT}')
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`)
 })
 
 
