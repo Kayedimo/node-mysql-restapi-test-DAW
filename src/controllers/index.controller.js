6adbec69eccc1e4a82233abf7efdfff04f318a73
Kayedimo <yessenia.dimo@gmail.com>
2025-11-10 17:00:25 +0100

    Se añadió manejo de errores en el controlador

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,12 @@
 import { pool } from '../db.js'
 
 export const  ping = async (req, res) => {
-    const [result] = await pool.query('SELECT "Pong" AS result')
-    res.json(result[0])
+    try {
+        const [result] = await pool.query('SELECT "Pong" AS result')
+        res.json(result[0])
+    } catch (error) {
+        console.log('Error en /ping: ', error)
+        res.status(500).json({ error: 'Error interno del servidor', detalle: error.message})
+        
+    }
 }
